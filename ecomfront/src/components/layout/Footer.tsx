7394753ebefa372be1e1b3538e1cf738f6b853b8
RealNavi <joseraulhmjr91220@gmail.com>
2025-10-05 15:54:15 -0600

    Cambios formulario footer

--- ecomfront/src/components/layout/Footer.tsx
+++ ecomfront/src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="h-auto py-10 bg-main text-white flex flex-col items-center font-quicksand px-10 space-y-10">
             <div className="flex w-[90%] justify-between">
                 <div className=" space-y-15  w-[33%]">
-                    <div className="flex justify-center mx-10 gap-2 text-xs font-light">
+                    <div className="flex justify-center mx-10 pt-10 gap-10 text-sm font-light">
                         <ul className="mr-10 space-y-4">
                             <li><a href="">Ofertas</a></li>
                             <li><a href="">Tiendas</a></li>
@@ -16,9 +16,14 @@ export default function Footer() {
                             <li><a href="">Vender</a></li>
                             <li><a href="">Lo más vendido</a></li>
                         </ul>
+                        <ul className="space-y-4">
+                            <li><a href="">Ayuda</a></li>
+                            <li><a href=""></a></li>
+                            <li><a href=""></a></li>
+                        </ul>
                     </div>
                     <div className="flex flex-col gap-3 items-center">
-                        <p>Síguenos en</p>
+                        <p className="font-semibold text-xl">Síguenos en</p>
                         <div className="flex gap-4">
                             <IconBrandFacebook className="" />
                             <IconBrandInstagram className="" />
@@ -28,15 +33,28 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className="flex flex-col items-center gap-2 w-[33%]">
-                    <img src={logo} alt="TucaShop" className="h-12 w-auto" />
-                    <a className="text-2xl font-fugaz" href="">TucaShop</a>
+                    <img src={logo} alt="TucaShop" className="h-15 w-auto" />
+                    <a className="text-3xl font-fugaz" href="">TucaShop</a>
                 </div>
-                <div className="flex flex-col gap-3 text-xs w-[33%] items-center">
-                    <p>Contactanos</p>
-                    <form className="flex flex-col gap-3 w-[90%]" action="" onSubmit={(e) => e.preventDefault()}>
-                        <input type="text" placeholder="Ingresa tu nombre" className="rounded-full p-2 text-white border border-white w-full" />
-                        <input type="email" placeholder="Ingresa tu email" className="rounded-full p-2 text-white border border-white w-full" />
-                        <textarea name="" placeholder="Ingresa tu mensaje" className="rounded-3xl p-2 text-white border border-white w-full h-20" />
+                <div className="flex flex-col gap-3 text-xs w-[33%] items-center pt-8">
+                    <p className="font-semibold text-xl">Contactanos</p>
+                    <form className="flex flex-col gap-4 w-[80%]" action="" onSubmit={(e) => e.preventDefault()}>
+                        <label htmlFor="" className="flex flex-col gap-1">
+                            <p className="pl-3 font-semibold">Nombre</p>
+                            <input type="text" placeholder="Ingresa tu nombre" className="rounded-full p-2 text-white border border-white w-full" />
+                        </label>
+                        <label htmlFor="" className="flex flex-col gap-1">
+                            <p className="pl-3 font-semibold">Email</p>
+                            <input type="email" placeholder="Ingresa tu email" className="rounded-full p-2 text-white border border-white w-full" />
+                        </label>
+                        <label htmlFor="" className="flex flex-col gap-1">
+                            <p className="pl-3 font-semibold">Asunto</p>
+                            <input type="text" placeholder="Escribe el asunto de tu mensaje" className="rounded-full p-2 text-white border border-white w-full" />
+                        </label>
+                        <label htmlFor="" className="flex flex-col gap-1">
+                            <p className="pl-3 font-semibold">Mensaje</p>
+                            <textarea name="" placeholder="Ingresa tu mensaje" className="rounded-2xl p-2 text-white border border-white w-full h-20" />
+                        </label>
                         <button type="submit" className="bg-contrast-secondary p-2 rounded-full hover:bg-contrast-main transition-all duration-300">Enviar</button>
                     </form>
 
