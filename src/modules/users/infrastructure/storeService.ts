bb26b24c3b7e7956249faf45ce4623fba97c8a71
JohnHito <johnjairo988@gmail.com>
2025-10-08 14:35:58 -0600

    Axios link fix

--- src/modules/users/infrastructure/storeService.ts
+++ src/modules/users/infrastructure/storeService.ts
@@ -1,10 +1,13 @@
-import api from "../../../lib/axios";
+
 import type { Store } from "./useUser";
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 export async function getStoreByUser(userId: number): Promise<Store | null> {
   try {
     const token = localStorage.getItem("access_token");
-    const { data } = await api.get(`/users/${userId}/store`, {
+    const { data } = await axios.get(`/users/${userId}/store`, {
       headers: { Authorization: `Bearer ${token}` },
     });
     return data || null;
