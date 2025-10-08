0166c761550511bb902a45081509a6bcb94a6dc5
JohnHito <johnjairo988@gmail.com>
2025-10-08 13:47:07 -0600

    a

--- src/hooks/useContactForm.ts
+++ src/hooks/useContactForm.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { ChangeEvent, FormEvent } from "react";
 import axios from "axios";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 interface ContactFields {
   name: string;
