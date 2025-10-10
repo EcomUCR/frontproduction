656081f4d4bf526b560d2211b2de4a2b7ea454ea
RealNavi <joseraulhmjr91220@gmail.com>
2025-10-09 18:30:36 -0600

    Banners

--- src/components/navigation/HeaderSlider.tsx
+++ src/components/navigation/HeaderSlider.tsx
@@ -1,6 +1,6 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./carousel";
 import banner from "../../img/resources/banner1.jpg";
-import banner3 from "../../img/resources/banner3.png";
+import banner6 from "../../img/resources/banner6.png";
 import banner4 from "../../img/resources/banner4.png";
 import banner5 from "../../img/resources/banner5.png";
 
@@ -8,10 +8,10 @@ export default function HeaderSlider() {
     return (
         <header>
             <Carousel className="mx-10">
-                <CarouselContent className="">
-                    <CarouselItem className="basis-full flex justify-center items-center" ><img src={banner} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
-                    <CarouselItem className="basis-full flex justify-center items-center" ><img src={banner3} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
+                <CarouselContent className="h-[30rem]">
+                    <CarouselItem className="basis-full flex justify-center items-center " ><img src={banner} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
                     <CarouselItem className="basis-full flex justify-center items-center"><img src={banner5} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
+                    <CarouselItem className="basis-full flex justify-center items-center" ><img src={banner6} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
                     <CarouselItem className="basis-full flex justify-center items-center"><img src={banner4} alt="" className="w-[90%] h-[80%] object-cover rounded-3xl"/></CarouselItem>
                 </CarouselContent>
                 <CarouselPrevious/>
