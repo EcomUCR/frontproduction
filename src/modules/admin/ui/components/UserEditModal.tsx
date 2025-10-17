7facb612644fe6926e6e1ad0c9181b2b4049c551
RealNavi <joseraulhmjr91220@gmail.com>
2025-10-17 15:31:53 -0600

    arreglo de botones

--- src/modules/admin/ui/components/UserEditModal.tsx
+++ src/modules/admin/ui/components/UserEditModal.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { IconArrowLeft, IconCamera, IconTrash } from "@tabler/icons-react";
+import { IconArrowLeft, IconCamera, IconEdit, IconTrash } from "@tabler/icons-react";
 import ButtonComponent from "../../../../components/ui/ButtonComponent";
 import { Switch } from "../../../../components/ui/switch";
 import { uploadImage } from "../../../users/infrastructure/imageService";
@@ -227,7 +227,7 @@ export default function UserEditModal({
               {/* Botones debajo de la imagen */}
               <div className="flex gap-3 mb-6">
                 <label className="bg-main text-white rounded-full px-3 py-1 cursor-pointer shadow-md hover:bg-main/80 transition text-sm flex items-center gap-1">
-                  <IconCamera size={16} /> Cambiar
+                  <IconEdit size={16} /> Editar
                   <input
                     type="file"
                     accept="image/*"
