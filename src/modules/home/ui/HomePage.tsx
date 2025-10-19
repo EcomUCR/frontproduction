b5ede6fa65682fd02d382383d14e75a94314b3e0
RealNavi <joseraulhmjr91220@gmail.com>
2025-10-18 22:45:58 -0600

    John NO

--- src/modules/home/ui/HomePage.tsx
+++ src/modules/home/ui/HomePage.tsx
@@ -156,7 +156,7 @@ export default function HomePage() {
         const b = activeBanners[0];
         return (
           <div className="flex justify-center items-center">
-            <div className="scale-[0.9] transition-transform duration-300">
+            <div className="transition-transform duration-300">
               <BannerComponent
                 {...b}
                 image={
